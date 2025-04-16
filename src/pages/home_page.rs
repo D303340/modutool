b51bb48506c75419691fc50972d800323e0c2dc0
D303340 <154211306+D303340@users.noreply.github.com>
2025-04-16 15:45:13 +0200

    Added button to home-page
    Home-page was a little too empty, so i added a "to schindler" button so the page has at least something.

--- src/pages/home_page.rs
+++ src/pages/home_page.rs
@@ -2,7 +2,13 @@ use crate::slint_ui::*;
 
 
 pub fn home_page(ui: &AppWindow, ui_weak: slint::Weak<AppWindow>){
-    let _home_page_logic = ui.global::<HomePageLogic>();
+    let home_page_logic = ui.global::<HomePageLogic>();
+    let ui_weak = ui_weak.clone();
 
-    let _ui_weak = ui_weak.clone();
+    home_page_logic.on_to_schindler(move || {    
+        if let Some(ui) = ui_weak.upgrade() {
+            ui.set_current_page(Pages::SchindlerPage);
+        }
+    });
+
 }
