eb61bc3db7a088bfcdf0259d25fa5fc7975572a1
D303340 <154211306+D303340@users.noreply.github.com>
2025-06-02 16:02:47 +0200

    added new components and settings page
    added a settings page, to change visual stuff such as text size or light/dark mode
    added a Modal component that can be used to display a message to the user, for example asking the user to confirm changes.

--- src/slint_ui.rs
+++ src/slint_ui.rs
@@ -15,13 +15,27 @@ pub fn to_schindler_page(ui: &AppWindow, ui_weak: slint::Weak<AppWindow>) {
     }
 }
 
+pub fn to_settings_page(ui: &AppWindow, ui_weak: slint::Weak<AppWindow>) {
+    let ui_weak = ui_weak.clone();
+    if let Some(ui) = ui_weak.upgrade() {
+        ui.set_current_page(Pages::SettingsPage);
+    }
+}
+
 pub fn HeaderLogic(ui: &AppWindow, ui_weak: slint::Weak<AppWindow>) {
     let Header = ui.global::<HeaderLogic>();
-    let ui_weak = ui_weak.clone();
     
+    let ui_weak_home = ui_weak.clone();
     Header.on_to_home_page(move || {
-        if let Some(ui) = ui_weak.clone().upgrade() {
-            to_home_page(&ui, ui_weak.clone());
+        if let Some(ui) = ui_weak_home.upgrade() {
+            to_home_page(&ui, ui_weak_home.clone());
+        }
+    });
+
+    let ui_weak_settings = ui_weak.clone();
+    Header.on_to_settings_page(move || {
+        if let Some(ui) = ui_weak_settings.upgrade() {
+            to_settings_page(&ui, ui_weak_settings.clone());
         }
     });
 
