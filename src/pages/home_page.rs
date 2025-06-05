6ecc5a50979fe2434225fdc10dd9986bf7ff637b
D303340 <154211306+D303340@users.noreply.github.com>
2025-06-05 16:03:44 +0200

    testing the Kollmorgen browser

--- src/pages/home_page.rs
+++ src/pages/home_page.rs
@@ -3,12 +3,30 @@ use slint::Model;
 use crate::slint_ui::*;
 
 
+pub fn to_kollmorgen(ui: &AppWindow, ui_weak: slint::Weak<AppWindow>) {
+    let ui_weak = ui_weak.clone();
+    if let Some(ui) = ui_weak.upgrade() {
+        if let Err(e) = open::that("192.168.100.1") {
+            eprintln!("Failed to open URL: {}", e);
+        }
+    }
+}
+
+
 pub fn home_page(ui: &AppWindow, ui_weak: slint::Weak<AppWindow>){
     let ui_weak = ui_weak.clone();
 
+    let ui_weak_schindler = ui_weak.clone();
     ui.global::<HomePageLogic>().on_to_schindler(move || {    
-        if let Some(ui) = ui_weak.upgrade() {
+        if let Some(ui) = ui_weak_schindler.upgrade() {
             ui.set_current_page(Pages::SchindlerPage);
+        }
+    });
+
+    let ui_weak_kollmorgen = ui_weak.clone();
+    ui.global::<HomePageLogic>().on_to_kollmorgen(move || {
+        if let Some(ui) = ui_weak_kollmorgen.upgrade() {
+            to_kollmorgen(&ui, ui_weak_kollmorgen.clone());
         }
     });
 
